6290cf68d774358dc06af68123b9477acfc62d24
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2025-01-21 09:32:17 +0100

    f3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSHARP25012101;
+
+const string PATH = "..\\..\\..\\RES\\fob2016.txt";
+List<Versenyzo> versenyzok = [];
+
+using StreamReader sr = new(PATH);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+Console.WriteLine($"3. feladat: versenyzok szama: {versenyzok.Count}");
+
